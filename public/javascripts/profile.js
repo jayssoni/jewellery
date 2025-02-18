dfdb61db0c7151ff9b5a7d0306407d4e5a5604fe
jay <jaykumarsoni944@gmail.com>
2025-02-18 13:45:20 +0530

    flash message 3

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -7,3 +7,15 @@
             }
         }
  
+
+        document.addEventListener("DOMContentLoaded", function () {
+            const flashMessage = document.getElementById("flash-message");
+            if (flashMessage) {
+                setTimeout(() => {
+                    flashMessage.style.transition = "opacity 0.5s";
+                    flashMessage.style.opacity = "0";
+                    setTimeout(() => flashMessage.remove(), 500);
+                }, 3000);
+            }
+        });
+        
