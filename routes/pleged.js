377bee83234f85a058ec3dd9d24f2fba9032fa4c
Mehu Kumar Sahu <mehul.www.mk@gmail.com>
2025-02-21 10:01:40 +0530

    history

--- routes/pleged.js
+++ routes/pleged.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
+const Invoice = require('../models/invoice');
 
-// Third page route
-router.get('/',authMiddleware, (req, res) => {
-  res.render('pleged', { user: req.user }); // Render the third.ejs template
+// Invoice history page route
+router.get('/', authMiddleware, async (req, res) => {
+    try {
+        const invoices = await Invoice.find({ user: req.user._id }).populate("user");
+        res.render('pleged', { user: req.user, invoices });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
